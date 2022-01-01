c5fbf9db9fe31826f76871eabe4ef72fb02027da
comtechnet <comtechnet@gmail.com>
2021-12-31 22:03:41 -0500

    Typechain conflicts with dup names due to Case

--- packages/theword-contracts/contracts/proxies/TheWordOfferingHouseProxy.sol
+++ packages/theword-contracts/contracts/proxies/TheWordOfferingHouseProxy.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
-/// @title The Nouns DAO offering house proxy
+/// @title The TheWord DAO offering house proxy
 
 /*********************************
  * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
@@ -19,7 +19,7 @@ pragma solidity ^0.8.6;
 
 import { TransparentUpgradeableProxy } from '@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol';
 
-contract NounsOfferingHouseProxy is TransparentUpgradeableProxy {
+contract TheWordOfferingHouseProxy is TransparentUpgradeableProxy {
     constructor(
         address logic,
         address admin,
